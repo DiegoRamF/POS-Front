61884eca2af891ac4d413190d95a3df9c47511c4
Diego Ramirez <diegorf339@gmail.com>
2026-06-21 17:41:17 -0500

    Revert "añadiendo Hero Section"
    This reverts commit 573196a8b9cd79601d92b947f7ac849891251be5.

--- src/app/pages/marketing/landing/landing.ts
+++ src/app/pages/marketing/landing/landing.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
 import { TopBar } from "../components/top-bar/top-bar";
 
 @Component({
   selector: 'landing',
-  imports: [ RouterLink, TopBar ],
+  imports: [TopBar],
   templateUrl: './landing.html',
 })
-export default class Landing {
-
-  scrollToPrices() {
-    const prices = document.getElementById( 'precios' );
-
-    if ( prices ) {
-      prices.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    };
-  };
-
-};
+export default class Landing {}
