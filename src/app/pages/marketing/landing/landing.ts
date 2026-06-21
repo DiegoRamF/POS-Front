f1fac4021f508347065d647d010feba77fb7f415
Diego Ramirez <diegorf339@gmail.com>
2026-06-21 17:49:35 -0500

    actualizando Hero Section

--- src/app/pages/marketing/landing/landing.ts
+++ src/app/pages/marketing/landing/landing.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { TopBar } from "../components/top-bar/top-bar";
 
 @Component({
   selector: 'landing',
-  imports: [TopBar],
+  imports: [ RouterLink, TopBar ],
   templateUrl: './landing.html',
 })
-export default class Landing {}
+export default class Landing {
+
+  scrollToPrices() {
+    const prices = document.getElementById( 'precios' );
+
+    if ( prices ) {
+      prices.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    };
+  };
+
+};
