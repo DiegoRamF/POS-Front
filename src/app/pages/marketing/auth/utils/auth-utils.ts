1686571ab64d9edd3af013bb6120fa3991689c89
Diego Ramirez <diegorf339@gmail.com>
2026-07-07 12:48:48 -0500

    Agregando validadores a register page

---
+++ src/app/pages/marketing/auth/utils/auth-utils.ts
@@ -0,0 +1,42 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+export const AUTH_REGEX = {
+  PHONE_PERU: /^\d{9}$/,
+  EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  PASSWORD_STRONG: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/
+};
+
+export class AuthValidators {
+
+  static emailFormat(): ValidatorFn {
+    return( control: AbstractControl ): ValidationErrors | null => {
+      if( !control.value ) return null;
+      const isValid = AUTH_REGEX.EMAIL.test( control.value );
+      return isValid ? null : { isValidEmail: true };
+    };
+  };
+
+  static phoneFormat(): ValidatorFn {
+    return ( control: AbstractControl ): ValidationErrors | null => {
+      if( !control.value ) return null;
+      const isValid = AUTH_REGEX.PHONE_PERU.test( control.value );
+      return isValid ? null : { invalidPhone: true };
+    };
+  };
+
+  static strongPassword(): ValidatorFn {
+    return ( control: AbstractControl ): ValidationErrors | null => {
+      if( !control.value ) return null;
+      const isValid = AUTH_REGEX.PASSWORD_STRONG.test( control.value );
+      return isValid ? null : { weakPassword: true}
+    };
+  };
+
+  static passwordsMatch: ValidatorFn = ( control: AbstractControl ): ValidationErrors | null => {
+    const password = control.get( 'password' )?.value;
+    const confirmPassword = control.get( 'confirmPassword' )?.value;
+    if( !password || !confirmPassword ) return null;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  };
+
+};
